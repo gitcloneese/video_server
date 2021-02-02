9d86b5ce0862304dd9d01eab9a8307707c5de503
panliangliang <panll@blizzmi.com>
2021-02-02 14:23:29 +0800

    修改

--- scheduler/main.go
+++ scheduler/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/gitcloneese/video_server/scheduler/taskrunner"
 	"github.com/julienschmidt/httprouter"
+	"github.com/gitcloneese/video_server/scheduler/taskrunner"
 )
 
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
-	router.GET("/video_delete-rec/:vid-id", vidDelRecHandler)
+
+	router.GET("/video-delete-record/:vid-id", vidDelRecHandler)
+
 	return router
+
 }
 
 func main() {
+	taskrunner.S
 	r := RegisterHandlers()
-	http.ListenAndServe(":9001", r)
+	http.ListenAndServe(":9000", r)
 }
